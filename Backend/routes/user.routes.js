74706a48e84537fc8931300099e4d258aa246950
MirzaSameer301 <sameerijaz301@gmail.com>
2024-11-29 22:34:54 +0500

    registrition done

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { body } from 'express-validator';
+import { registerUser } from '../controllers/user.controller.js';
+const router =express.Router();
+
+router.post('/register',[
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('fullname.firstname').isLength({min:3}).withMessage("First name must be atleast 3 letters"),
+    body('password').isLength({min:6}).withMessage("Password must be atleast 6 letters")
+],
+registerUser
+)
+
+export default router;
