fa4352b853b6e79ad4c4db570b26416cd5fee09f
MirzaSameer301 <sameerijaz301@gmail.com>
2024-11-29 22:34:13 +0500

    registrition done

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,18 @@ import dotenv from 'dotenv'
 dotenv.config();
 import cors from 'cors'
 import { connectToDb } from './db/db.js';
+import userRoutes from './routes/user.routes.js'
+
 connectToDb();
 const app=express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.send("Hello")
 });
+app.use('/users',userRoutes);
 
 export default app;
